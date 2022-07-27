bf65bd7e736a1f8905c2c0a2b0814528b12e3961
Lucas <lucas.compiegne1@gmail.com>
2022-07-27 10:25:34 +0200

    fix ex04 ex05

--- C_03_Rendu/ex04/ft_strstr.c
+++ C_03_Rendu/ex04/ft_strstr.c
@@ -6,44 +6,26 @@
 /*   By: lcompieg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 22:55:56 by lcompieg          #+#    #+#             */
-/*   Updated: 2022/07/17 22:56:12 by lcompieg         ###   ########lyon.fr   */
+/*   Updated: 2022/07/27 10:03:27 by lcompieg         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	str_len(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
 char	*ft_strstr(char *str, char *to_find)
 {
-	int	index;
-	int	same_char;
+	int	i;
+	int	j;
 
-	index = 0;
-	if (str_len(to_find) == 0)
-		return (&str[index]);
-	if (str_len(str) < str_len(to_find) || str_len(str) == 0)
-		return (0);
-	while (str[index])
+	i = 0;
+	if (to_find[i] == '\0')
+		return (str);
+	while (str[i] != '\0')
 	{
-		if (str[index] == to_find[0])
-		{
-			same_char = 1;
-			while (str[index + same_char] == to_find[same_char]
-				&& same_char >= 1)
-			{
-				if (to_find[same_char] && same_char == str_len(to_find) - 1)
-					return (&str[index]);
-				same_char++;
-			}
-		}
-		index++;
+		j = 0;
+		while (str[i + j] == to_find[j] && str[i + j] != '\0')
+			j++;
+		if (to_find[j] == '\0')
+			return (&str[i]);
+		i++;
 	}
 	return (0);
 }
--- C_03_Rendu/ex05/ft_strlcat.c
+++ C_03_Rendu/ex05/ft_strlcat.c
@@ -6,29 +6,45 @@
 /*   By: lcompieg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 22:56:36 by lcompieg          #+#    #+#             */
-/*   Updated: 2022/07/17 22:58:08 by lcompieg         ###   ########lyon.fr   */
+/*   Updated: 2022/07/27 10:16:15 by lcompieg         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	unsigned int	destlen;
-	unsigned int	srclen;
-	unsigned int	index;
+	unsigned int	dest_len;
+	unsigned int	src_len;
+	int				i;
+	int				j;
 
-	destlen = 0;
-	index = 0;
-	srclen = 0;
-	while (dest[destlen] && destlen < size)
-		destlen++;
-	while (src[srclen])
-		srclen++;
-	while (src[index] && (destlen + index) < size - 1)
+	dest_len = ft_strlen(dest);
+	src_len = ft_strlen(src);
+	i = 0;
+	if (size < dest_len)
+		return (size + src_len);
+	while (dest[i] != '\0' && size > 0)
 	{
-		dest[destlen + index] = src[index];
-		index++;
+		i++;
+		size--;
 	}
-	if (src[index] == '\0')
-		dest[destlen + index] = '\0';
-	return (destlen + srclen);
+	j = 0;
+	while (src[j] != '\0' && size > 1)
+	{
+		dest[i + j] = src[j];
+		size--;
+		j++;
+	}
+	if (size > 0)
+		dest[i + j] = '\0';
+	return (dest_len + src_len);
 }
